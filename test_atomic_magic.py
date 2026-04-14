cf34c152810878750aa3eb67c5bd56ae3736128a
Trente Trois <jmorel33@hotmail.com>
2026-04-14 12:21:44 -0400

    Delete test_atomic_magic.py

--- test_atomic_magic.py
+++
@@ -1,5 +0,0 @@
-with open("mybuddy.h", "r") as f:
-    lines = f.readlines()
-for i, line in enumerate(lines):
-    if "magic" in line and "_Atomic" in line:
-        print(f"{i+1}: {line.strip()}")
